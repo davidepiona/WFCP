58b65ff5f7db37f9fb1d6f688fad953f33c4782b
Davide <davide.piona@studenti.unipd.it>
2018-04-05 13:03:10 +0200

    Fixed path problem script bash

--- src/multi_wfcp.sh
+++ src/multi_wfcp.sh
@@ -1,16 +1,19 @@
 # WFCP
-cd "/home/davide/Scrivania/WFCP/data"
-for t in `find . -type d -name 'wf02' | cut -c 3-`
+make
+cd ..
+path=$(pwd)
+cd ${path}'/data'
+for t in `find . -type d -name 'w*' | cut -c 3-`
 do
 	echo ${t}	
-	cd "/home/davide/Scrivania/WFCP/data/"${t}
+	cd ${path}'/data/'${t}
 	pwd
 	for c in `find . -type f -name '*.cbl' | cut -c 3-`
 	do
 		echo "---"$c
-		cd "/home/davide/Scrivania/WFCP/src"
-		./wfcp -ft '/home/davide/Scrivania/WFCP/data/'${t}'/'${t}'.turb' -fc '/home/davide/Scrivania/WFCP/data/'${t}'/'${c} -C 10 -time_limit 15 -rins 10 -relax 1 > ../runs/run_${c}.log
+		cd ${path}'/src'
+		./wfcp -ft ${path}'/data/'${t}'/'${t}'.turb' -fc ${path}'/data/'${t}'/'${c} -C 10 -time_limit 15 -rins 10 -relax 1 > ../runs/run_${c}.log
 	done
 done
-cd "/home/davide/Scrivania/WFCP/runs"
+cd ${path}'/data/runs'
 grep "STAT" *.log |sort > results.csv
