3dabe252eacf7ff365fb69eed44d0e5d9ab0995e
Davide <davide.piona@studenti.unipd.it>
2018-04-05 02:36:36 +0200

    Improved script shell: working with all the folders

--- src/multi_wfcp.sh
+++ src/multi_wfcp.sh
@@ -1,10 +1,16 @@
 # WFCP
-cd "/home/davide/Scrivania/WFCP/data/wf01"
-for F in `find . -type f -name '*.cbl' | cut -c 3-`
+cd "/home/davide/Scrivania/WFCP/data"
+for t in `find . -type d -name 'w*' | cut -c 3-`
 do
-echo ${F}
-cd "/home/davide/Scrivania/WFCP/src"
-./wfcp -ft '/home/davide/Scrivania/WFCP/data/wf01/wf01.turb' -fc '/home/davide/Scrivania/WFCP/data/wf01/'$F -C 10 -time_limit 100 -rins 10 -relax 1 > run_wf01_$F.log
+	echo ${t}	
+	cd ${t}
+	pwd
+	for c in `find . -type f -name '*.cbl' | cut -c 3-`
+	do
+		echo "---"$c
+		cd "/home/davide/Scrivania/WFCP/src"
+		./wfcp -ft '/home/davide/Scrivania/WFCP/data/'${t}'/'${t}'.turb' -fc '/home/davide/Scrivania/WFCP/data/'${t}'/'${c} -C 10 -time_limit 15 -rins 10 -relax 1 > run_${c}.log
+	done
+	cd ../data
 done
-
 grep "STAT" *.log |sort > results.csv
