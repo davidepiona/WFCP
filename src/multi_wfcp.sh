376662338f982e0bec962dfc4362b400a4c8658d
Davide <davide.piona@studenti.unipd.it>
2018-04-05 02:49:53 +0200

    Little changes in script shell

--- src/multi_wfcp.sh
+++ src/multi_wfcp.sh
@@ -1,16 +1,16 @@
 # WFCP
 cd "/home/davide/Scrivania/WFCP/data"
-for t in `find . -type d -name 'w*' | cut -c 3-`
+for t in `find . -type d -name 'wf02' | cut -c 3-`
 do
 	echo ${t}	
-	cd ${t}
+	cd "/home/davide/Scrivania/WFCP/data/"${t}
 	pwd
 	for c in `find . -type f -name '*.cbl' | cut -c 3-`
 	do
 		echo "---"$c
 		cd "/home/davide/Scrivania/WFCP/src"
-		./wfcp -ft '/home/davide/Scrivania/WFCP/data/'${t}'/'${t}'.turb' -fc '/home/davide/Scrivania/WFCP/data/'${t}'/'${c} -C 10 -time_limit 15 -rins 10 -relax 1 > run_${c}.log
+		./wfcp -ft '/home/davide/Scrivania/WFCP/data/'${t}'/'${t}'.turb' -fc '/home/davide/Scrivania/WFCP/data/'${t}'/'${c} -C 10 -time_limit 15 -rins 10 -relax 1 > ../runs/run_${c}.log
 	done
-	cd ../data
 done
+cd "/home/davide/Scrivania/WFCP/runs"
 grep "STAT" *.log |sort > results.csv
