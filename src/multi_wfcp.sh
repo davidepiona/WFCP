3469eef302252df035a5d3e5d4d816d17bcbdffc
Davide <davide.piona@studenti.unipd.it>
2018-04-20 16:27:14 +0200

    Little fix after the last push

--- src/multi_wfcp.sh
+++ src/multi_wfcp.sh
@@ -11,7 +11,7 @@ for c in `find . -type f -name '*.turb' | cut -c 3-9 |sort`
 do
 	echo "---"$c
 	cd ${path}'/src'
-	./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' -C 10 -rins 5 -relax 1 -gap 0.1 -seed 9 -CC 3 -time_limit 30 > ../runs/${dir}/run_${c}.log
+	./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' -C 10 -rins 5 -relax 1 -gap 0.1 -seed 9 -CC 3 -time_limit 30 -names 1 > ../runs/${dir}/run_${c}.log
 done
 #mv *.png '../runs/'${dir}
 mv plot '../runs/'${dir}
--- src/single_wfcp.sh
+++ src/single_wfcp.sh
@@ -9,5 +9,6 @@ c="data_03"
 mkdir runs/${dir}
 path=$(pwd)
 cd ${path}'/src'
-./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' -C 3 -time_limit 150 -rins 10 -relax 1 -polishing_time 150 -CC 1
-
+./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' -C 10 -time_limit 50 -rins 5 -relax 1 -polishing_time 150 -seed 9 -gap 0.1 -CC 3 -names 1
+mv plot '../runs/'${dir}
+mkdir plot
