61b4cc15d68b9c9866e2b38a3043130777b37fc8
Davide <davide.piona@studenti.unipd.it>
2018-04-25 13:07:49 +0200

    the multi_wfcp script save also the settings of the run

--- src/multi_wfcp.sh
+++ src/multi_wfcp.sh
@@ -6,15 +6,17 @@ cd ..
 dir=$(date +"%Y-%m-%d_%H-%M-%S")
 mkdir runs/${dir}
 path=$(pwd)
+settings="-C 10 -rins 5 -relax 1 -seed 9 -CC 3 -time_limit 30 -polishing_time 260 -names 1"
 cd ${path}'/data'
 for c in `find . -type f -name '*.turb' | cut -c 3-9 |sort`
 do
 	echo "---"$c
 	cd ${path}'/src'
-	./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' -C 10 -rins 5 -relax 1 -gap 0.1 -seed 9 -CC 3 -time_limit 30 -names 1 > ../runs/${dir}/run_${c}.log
+	./wfcp -ft ${path}'/data/'${c}'.turb' -fc ${path}'/data/'${c}'.cbl' ${settings} > ../runs/${dir}/run_${c}.log
 done
-#mv *.png '../runs/'${dir}
+mv *.png '../runs/'${dir}
 mv plot '../runs/'${dir}
 mkdir plot
 cd ${path}'/runs/'${dir}
+echo ${settings} > settings.txt
 grep "STAT" *.log |sort > results.csv
--- src/plot/script_plot.p
+++
@@ -1,5 +0,0 @@
-set autoscale
-plot \
-	'plot/plot_datastd_2.dat' using 1:2 with lines lc rgb "#0000FF" lw 2 title "Cable 3",\
-	'plot/plot_datastd_2.dat' using 1:2:(0.6) with circles fill solid lc rgb "black" notitle,\
-	'plot/plot_datastd_2.dat' using 1:2:3     with labels tc rgb "black" offset (0,0) font 'Arial Bold' notitle
